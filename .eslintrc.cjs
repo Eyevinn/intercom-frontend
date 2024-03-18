0fff882373fe3c32f667450599d9d1543d06487e
Martin <901824+martinstark@users.noreply.github.com>
2024-03-18 14:44:19 +0100

    chore: add and run prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,36 +2,36 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
     "plugin:jsx-a11y/recommended",
     "airbnb",
-    'airbnb-typescript',
+    "airbnb-typescript",
     "airbnb/hooks",
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  "parserOptions": {
-    "sourceType": "module",
-    project: ['./tsconfig.json']
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module",
+    project: ["./tsconfig.json"],
   },
-  plugins: ['react-refresh'],
-  "settings": {
-    "react": {
-      "version": "detect"
+  plugins: ["react-refresh"],
+  settings: {
+    react: {
+      version: "detect",
     },
-    "propWrapperFunctions": [],
-    "linkComponents": []
+    propWrapperFunctions: [],
+    linkComponents: [],
   },
   rules: {
     "react/react-in-jsx-scope": "off",
     "react/jsx-uses-react": "off",
     "import/extensions": "off",
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
-}
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '../../../../../vite.svg';
-import './App.css';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "../../../../../vite.svg";
+import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -19,16 +19,10 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button type="button" onClick={() => setCount((v) => v + 1)}>
-          count is
-          {' '}
-          {count}
+          count is {count}
         </button>
         <p>
-          Edit
-          {' '}
-          <code>src/App.tsx</code>
-          {' '}
-          and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
