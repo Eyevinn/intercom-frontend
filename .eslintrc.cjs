f00f639adb8f9c8921997f2f6d57f6845eeca0e9
Martin <901824+martinstark@users.noreply.github.com>
2024-03-18 14:38:48 +0100

    chore: update linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,11 +4,19 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    "plugin:jsx-a11y/recommended",
+    "airbnb",
+    'airbnb-typescript',
+    "airbnb/hooks",
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  "parserOptions": {
+    "sourceType": "module",
+    project: ['./tsconfig.json']
+  },
   plugins: ['react-refresh'],
   "settings": {
     "react": {
@@ -20,6 +28,7 @@ module.exports = {
   rules: {
     "react/react-in-jsx-scope": "off",
     "react/jsx-uses-react": "off",
+    "import/extensions": "off",
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
