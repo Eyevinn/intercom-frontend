f93c5e322fed9d785f9bad7da8584df540990643
Martin <901824+martinstark@users.noreply.github.com>
2024-03-18 11:22:22 +0100

    feat: add emotion, gitignore

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import styled from '@emotion/styled'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+const Heh = styled.button`
+  background: red;
+`
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Heh />
     <App />
   </React.StrictMode>,
 )
