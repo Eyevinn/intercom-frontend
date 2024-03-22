d7733c85b41e19a50ba52cab5f88c6e2a22fbe21
Martin <901824+martinstark@users.noreply.github.com>
2024-03-22 13:38:03 +0100

    feat: update routing syntax, add global state context

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import { Production } from "./components/production.tsx";
+import { Production } from "./components/production/production.tsx";
 import { ErrorPage } from "./components/router-error.tsx";
 import { useDevicePermissions } from "./hooks/device-permission.ts";
 import { LandingPage } from "./components/landing-page/landing-page.tsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <LandingPage />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "production/:productionId",
-    // loader: productionLoader,
-    element: <Production />,
-  },
-]);
+import { useInitializeGlobalStateReducer } from "./global-state/global-state-reducer.ts";
+import { GlobalStateContext } from "./global-state/context-provider.tsx";
 
 const App = () => {
   const { denied, permission } = useDevicePermissions();
+  const initializedGlobalState = useInitializeGlobalStateReducer();
 
   if (denied) {
     return (
@@ -31,7 +21,24 @@ const App = () => {
 
   if (!permission) return <div>Waiting for device permissions</div>;
 
-  return <RouterProvider router={router} />;
+  return (
+    <GlobalStateContext.Provider value={initializedGlobalState}>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={<LandingPage />}
+            errorElement={<ErrorPage />}
+          />
+          <Route
+            path="/production/:productionId"
+            element={<Production />}
+            errorElement={<ErrorPage />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </GlobalStateContext.Provider>
+  );
 };
 
 export default App;
