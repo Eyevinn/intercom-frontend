bf631334c11b328237904aa60d23d5e12e50d5cf
Martin <901824+martinstark@users.noreply.github.com>
2024-03-22 15:47:34 +0100

    feat: connect production view to global state

--- src/global-state/types.ts
+++ src/global-state/types.ts
@@ -2,4 +2,6 @@ import { TProduction } from "../components/production/types.ts";
 
 export type TGlobalState = {
   production: TProduction | null;
+  //  selectedInput: string | null;
+  //  selectedOutput: string | null;
 };
