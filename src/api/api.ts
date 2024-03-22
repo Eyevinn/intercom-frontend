9a1af29ca90716bfb869a9093c7fa4314793e118
Martin <901824+martinstark@users.noreply.github.com>
2024-03-22 17:23:42 +0100

    feat: add api structure

---
+++ src/api/api.ts
@@ -0,0 +1,55 @@
+// TODO env variable
+const rootUrl = "https://intercom-manager.dev.eyevinn.technology/";
+
+type TCreateProductionOptions = {
+  name: string;
+  lines: string[];
+};
+
+type TCreateProductionResponse = {
+  productionId: string;
+};
+
+// TODO create generic response/error converter and response data validator
+export const API = {
+  createProduction: ({
+    name,
+    lines,
+  }: TCreateProductionOptions): Promise<TCreateProductionResponse> =>
+    fetch(`${rootUrl}production/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        lines: lines.map((line) => ({
+          name: line,
+        })),
+      }),
+    }).then((response) => response.json()),
+  // TODO add response types, headers
+  listProductions: () =>
+    fetch(`${rootUrl}productions/`, { method: "GET" }).then((response) =>
+      response.json()
+    ),
+  fetchProduction: (id: number) =>
+    fetch(`${rootUrl}productions/${id}`, { method: "GET" }).then((response) =>
+      response.json()
+    ),
+  deleteProduction: (id: number) =>
+    fetch(`${rootUrl}productions/${id}`, { method: "DELETE" }).then(
+      (response) => response.json()
+    ),
+  listProductionLines: (id: number) =>
+    fetch(`${rootUrl}productions/${id}/lines`, { method: "GET" }).then(
+      (response) => response.json()
+    ),
+  fetchProductionLine: (productionId: number, lineId: number) =>
+    fetch(`${rootUrl}productions/${productionId}/lines/${lineId}`, {
+      method: "GET",
+    }).then((response) => response.json()),
+  offerAudioSession: () => Promise.resolve(),
+  patchAudioSession: () => Promise.resolve(),
+  deleteAudioSession: () => Promise.resolve(),
+};
