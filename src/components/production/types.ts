3a51b9508dff681f0223392451b1da8e8df515d8
Martin <901824+martinstark@users.noreply.github.com>
2024-03-22 14:50:27 +0100

    chore: add ids to dummy production api

--- src/components/production/types.ts
+++ src/components/production/types.ts
@@ -5,6 +5,7 @@ export type TParticipant = {
 
 export type TLine = {
   name: string;
+  id: number;
   connected: boolean;
   connectionId: string;
   participants: TParticipant[];
@@ -12,5 +13,6 @@ export type TLine = {
 
 export type TProduction = {
   name: string;
+  id: number;
   lines: TLine[];
 };
