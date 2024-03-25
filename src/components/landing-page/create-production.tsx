7164816f490cb0ee889847ecd5356c0e3e055fcc
Martin <901824+martinstark@users.noreply.github.com>
2024-03-25 13:55:20 +0100

    feat: make create production form work

--- src/components/landing-page/create-production.tsx
+++ src/components/landing-page/create-production.tsx
@@ -1,3 +1,5 @@
+import { SubmitHandler, useFieldArray, useForm } from "react-hook-form";
+import { useState } from "react";
 import { DisplayContainerHeader } from "./display-container-header.tsx";
 import {
   DecorativeLabel,
@@ -6,24 +8,87 @@ import {
   FormLabel,
   SubmitButton,
 } from "./form-elements.tsx";
+import { API } from "../../api/api.ts";
 
+type FormValues = {
+  productionName: string;
+  defaultLine: string;
+  lines: { name: string }[];
+};
 export const CreateProduction = () => {
+  const [createdProductionId, setCreatedProductionId] = useState<string | null>(
+    null
+  );
+  const {
+    formState: { errors },
+    control,
+    register,
+    handleSubmit,
+  } = useForm<FormValues>();
+  const { fields, append } = useFieldArray({
+    control,
+    name: "lines",
+    rules: {
+      minLength: 1,
+    },
+  });
+
+  const onSubmit: SubmitHandler<FormValues> = (value) => {
+    API.createProduction({
+      name: value.productionName,
+      lines: [{ name: value.defaultLine }, ...value.lines],
+    })
+      .then((v) => setCreatedProductionId(v.productionid))
+      .catch((e) => {
+        console.error(e);
+        // TODO error handling, display error in form or in global header?
+      });
+  };
+
   return (
     <FormContainer>
       <DisplayContainerHeader>Create Production</DisplayContainerHeader>
       <FormLabel>
         <DecorativeLabel>Production Name</DecorativeLabel>
-        <FormInput type="text" value="" placeholder="Production Name" />
+        <FormInput
+          // eslint-disable-next-line
+          {...register(`productionName`, { minLength: 1 })}
+          placeholder="Production Name"
+        />
       </FormLabel>
+      {errors.productionName && <div>BAD INPUT</div>}
       <FormLabel>
         <DecorativeLabel>Line</DecorativeLabel>
-        <FormInput type="text" value="Editorial" placeholder="Line Name" />
+        <FormInput
+          // eslint-disable-next-line
+          {...register(`defaultLine`)}
+          type="text"
+          value="Editorial"
+          placeholder="Line Name"
+        />
       </FormLabel>
-      <FormLabel>
-        <DecorativeLabel>Line</DecorativeLabel>
-        <FormInput type="text" value="Production" placeholder="Line Name" />
-      </FormLabel>
-      <SubmitButton type="submit">Create Production</SubmitButton>
+      {fields.map((field, index) => (
+        <FormLabel key={field.id}>
+          <DecorativeLabel>Line</DecorativeLabel>
+          <FormInput
+            // eslint-disable-next-line
+            {...register(`lines.${index}.name`)}
+            type="text"
+          />
+        </FormLabel>
+      ))}
+      <button type="button" onClick={() => append({ name: "" })}>
+        Add
+      </button>
+
+      <SubmitButton type="submit" onClick={handleSubmit(onSubmit)}>
+        Create Production
+      </SubmitButton>
+      {createdProductionId !== null && (
+        <DecorativeLabel>
+          The production ID is: {createdProductionId.toString()}
+        </DecorativeLabel>
+      )}
     </FormContainer>
   );
 };
