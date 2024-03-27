4c55a5730bd24e2c6dff6262ec323922e3c1d818
Martin <901824+martinstark@users.noreply.github.com>
2024-03-27 16:11:59 +0100

    feat: add hook to fetch audio devices into global state

--- src/global-state/types.ts
+++ src/global-state/types.ts
@@ -3,6 +3,7 @@ import { TProduction } from "../components/production/types.ts";
 export type TGlobalState = {
   production: TProduction | null;
   error: Error | null;
+  devices: MediaDeviceInfo[] | null;
   //  selectedInput: string | null;
   //  selectedOutput: string | null;
 };
