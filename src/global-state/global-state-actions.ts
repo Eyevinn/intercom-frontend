746e1eb5983bd6d528fc22fcd3255193404345f0
malmen237 <linda.malm@eyevinn.se>
2024-03-27 10:41:03 +0100

    chore: added error-handling to global state reducer

--- src/global-state/global-state-actions.ts
+++ src/global-state/global-state-actions.ts
@@ -1,8 +1,14 @@
 import { TProduction } from "../components/production/types.ts";
+import { TError } from "./types.ts";
 
-export type TGlobalStateAction = TUpdateProductionAction;
+export type TGlobalStateAction = TUpdateProductionAction | TPublishError;
 
 export type TUpdateProductionAction = {
   type: "UPDATE_PRODUCTION";
   payload: TProduction;
+};
+
+export type TPublishError = {
+  type: "ERROR";
+  payload: TError;
 };
--- src/global-state/types.ts
+++ src/global-state/types.ts
@@ -1,7 +1,13 @@
 import { TProduction } from "../components/production/types.ts";
 
-export type TGlobalState = {
+export type TGlobalState = TGlobalProductionState & TError;
+
+export type TGlobalProductionState = {
   production: TProduction | null;
   //  selectedInput: string | null;
   //  selectedOutput: string | null;
+};
+
+export type TError = {
+  error: Error | null;
 };
