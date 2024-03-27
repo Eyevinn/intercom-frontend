7906690c80223069b24f0ac471152e6cf24f9793
Martin <901824+martinstark@users.noreply.github.com>
2024-03-27 16:12:32 +0100

    feat: add fetch production hook

---
+++ src/hooks/fetch-production.ts
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import { API } from "../api/api";
+import { TProduction } from "../components/production/types";
+
+type TUseFetchProduction = (id: number | null) => {
+  production: TProduction | null;
+  error: Error | null;
+};
+
+export const useFetchProduction: TUseFetchProduction = (id) => {
+  const [production, setProduction] = useState<TProduction | null>(null);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    let aborted = false;
+
+    if (id) {
+      API.fetchProduction(id)
+        .then((p) => {
+          if (aborted) return;
+
+          setError(null);
+
+          setProduction({
+            name: p.name,
+            id: parseInt(p.productionid, 10),
+            lines: p.lines.map((line) => ({
+              name: line.name,
+              id: parseInt(line.id, 10),
+              participants: [],
+              connected: false,
+            })),
+          });
+        })
+        .catch((e) => {
+          setProduction(null);
+
+          setError(e);
+        });
+    }
+
+    return () => {
+      aborted = true;
+    };
+  }, [id]);
+
+  return {
+    error,
+    production,
+  };
+};
