1a54a7e0c206126f9645b1518c134cb089a7a416
Martin <901824+martinstark@users.noreply.github.com>
2024-03-28 12:14:02 +0100

    feat: navigate to production based on new criteria

--- src/components/landing-page/use-navigate-to-production.ts
+++ src/components/landing-page/use-navigate-to-production.ts
@@ -4,13 +4,14 @@ import { useGlobalState } from "../../global-state/context-provider";
 
 // Navigates to a production line as soon as a new production is pushed to the global state
 export const useNavigateToProduction = () => {
-  const [{ production }] = useGlobalState();
+  const [{ joinProductionOptions }] = useGlobalState();
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (production) {
-      // TODO pick selected line id here
-      navigate(`/production/${production.id}/line/1`);
+    if (joinProductionOptions) {
+      navigate(
+        `/production/${joinProductionOptions.productionId}/line/${joinProductionOptions.lineId}`
+      );
     }
-  }, [navigate, production]);
+  }, [navigate, joinProductionOptions]);
 };
