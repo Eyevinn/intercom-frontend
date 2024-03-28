114c86ce7c8bd9e035f3a92405e69d2c33496c26
Martin <901824+martinstark@users.noreply.github.com>
2024-03-28 11:09:15 +0100

    feat: move flex container to generic components file

--- src/components/landing-page/landing-page.tsx
+++ src/components/landing-page/landing-page.tsx
@@ -3,17 +3,14 @@ import { JoinProduction } from "./join-production.tsx";
 import { CreateProduction } from "./create-production.tsx";
 import { ProductionsList } from "./productions-list.tsx";
 import { useNavigateToProduction } from "./use-navigate-to-production.ts";
-
-const FlexContainer = styled.div`
-  display: flex;
-`;
+import { FlexContainer } from "../generic-components.ts";
 
 const DisplayContainer = styled.div`
   flex-basis: 100%;
   display: flex;
 
   &:first-of-type {
-    border-right: 1px solid white;
+    border-right: 1px solid #424242;
     padding: 5rem 2rem 5rem 5rem;
     justify-content: flex-end;
   }
