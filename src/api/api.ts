0ed88a998918a6b4234d02206af4b81b959841d2
Martin <901824+martinstark@users.noreply.github.com>
2024-03-31 16:06:20 +0200

    feat: add delete session endpoint

--- src/api/api.ts
+++ src/api/api.ts
@@ -35,13 +35,9 @@ const handleFetchRequest = async <T>(
 
   const contentType = response.headers.get("content-type");
 
-  if (!contentType) {
-    throw new Error("No content-type on response. Report to your developer.");
-  }
-
-  if (contentType.indexOf("text/plain") > -1) {
+  if (contentType && contentType.indexOf("text/plain") > -1) {
     text = await response.text();
-  } else if (contentType.indexOf("application/json") > -1) {
+  } else if (contentType && contentType.indexOf("application/json") > -1) {
     json = await response.json();
   }
 
@@ -62,10 +58,6 @@ const handleFetchRequest = async <T>(
   }
 
   console.log("Request response:", json);
-
-  if (!json) {
-    throw new Error(`No response data. Response text: ${text}`);
-  }
 
   return json;
 };
@@ -91,6 +83,12 @@ type TPatchAudioSessionOptions = {
 type TPatchAudioSessionResponse = {
   sdp: string;
   sessionid: string;
+};
+
+type TDeleteAudioSessionOptions = {
+  productionId: number;
+  lineId: number;
+  sessionId: string;
 };
 
 export const API = {
@@ -153,5 +151,15 @@ export const API = {
         }
       )
     ),
-  deleteAudioSession: () => Promise.resolve(),
+  deleteAudioSession: ({
+    productionId,
+    lineId,
+    sessionId,
+  }: TDeleteAudioSessionOptions): Promise<string> =>
+    handleFetchRequest<string>(
+      fetch(
+        `${rootUrl}productions/${productionId}/lines/${lineId}/session/${sessionId}`,
+        { method: "DELETE" }
+      )
+    ),
 };
