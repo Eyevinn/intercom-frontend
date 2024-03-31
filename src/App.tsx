13bd4a0e32eab4606eb3791379466e28527fc7b6
Martin <901824+martinstark@users.noreply.github.com>
2024-03-31 17:08:45 +0200

    chore: move error file

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { LandingPage } from "./components/landing-page/landing-page.tsx";
 import { useInitializeGlobalStateReducer } from "./global-state/global-state-reducer.ts";
 import { GlobalStateContext } from "./global-state/context-provider.tsx";
 import { Header } from "./components/landing-page/header.tsx";
-import { Error } from "./components/landing-page/error.tsx";
+import { Error } from "./components/error";
 import { useFetchDevices } from "./use-fetch-devices.ts";
 import { FlexContainer } from "./components/generic-components.ts";
 import { DisplayWarning } from "./components/display-box.tsx";
