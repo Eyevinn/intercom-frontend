71aec13218a9c28c235a4341205fff06dc3174ec
Martin <901824+martinstark@users.noreply.github.com>
2024-04-04 10:17:58 +0200

    fix: restore interval type

--- src/components/production-line/production-line.tsx
+++ src/components/production-line/production-line.tsx
@@ -47,7 +47,7 @@ export const ProductionLine: FC = () => {
   useEffect(() => {
     if (!joinProductionOptions) return noop;
 
-    let interval: number = 0;
+    let interval: number | null = null;
 
     const productionId = parseInt(joinProductionOptions.productionId, 10);
     const lineId = parseInt(joinProductionOptions.lineId, 10);
