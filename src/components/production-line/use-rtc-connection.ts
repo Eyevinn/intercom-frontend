e353d53f40d9b201553471bbb11cdbf43e91e2e1
malmen237 <linda.malm@eyevinn.se>
2024-04-08 14:49:58 +0200

    feat: added dispatch to when the mediastream track is connected

--- src/components/production-line/use-rtc-connection.ts
+++ src/components/production-line/use-rtc-connection.ts
@@ -25,15 +25,21 @@ type TEstablishConnection = {
 type TAttachAudioStream = {
   inputAudioStream: MediaStream;
   rtcPeerConnection: RTCPeerConnection;
+  dispatch: Dispatch<TGlobalStateAction>;
 };
 
 const attachInputAudioToPeerConnection = ({
   inputAudioStream,
   rtcPeerConnection,
+  dispatch,
 }: TAttachAudioStream) =>
-  inputAudioStream
-    .getTracks()
-    .forEach((track) => rtcPeerConnection.addTrack(track));
+  inputAudioStream.getTracks().forEach((track) => {
+    rtcPeerConnection.addTrack(track);
+    dispatch({
+      type: "CONNECTED_MEDIASTREAM_TRACK",
+      payload: track,
+    });
+  });
 
 const establishConnection = ({
   rtcPeerConnection,
@@ -197,6 +203,7 @@ export const useRtcConnection = ({
       attachInputAudioToPeerConnection({
         rtcPeerConnection,
         inputAudioStream,
+        dispatch,
       });
     }
 
