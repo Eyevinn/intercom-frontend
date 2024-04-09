f4a433df2a7a42cce16f1ba5e9e69f3b989e2010
malmen237 <linda.malm@eyevinn.se>
2024-04-09 07:45:30 +0200

    fix: updated loader with classnames

--- src/components/loader/loader.tsx
+++ src/components/loader/loader.tsx
@@ -2,9 +2,6 @@ import styled from "@emotion/styled";
 import { FC } from "react";
 
 const Loading = styled.div`
-  position: absolute;
-  top: 5px;
-  left: 60px;
   border: 4px solid rgba(0, 0, 0, 0.1);
   border-top: 4px solid #333;
   border-radius: 50%;
@@ -12,6 +9,17 @@ const Loading = styled.div`
   height: 30px;
   animation: spin 1s linear infinite;
   margin: auto;
+
+  &.create-production {
+    position: absolute;
+    top: 5px;
+    left: 60px;
+  }
+
+  &.join-production {
+    border: 4px solid rgba(201, 201, 201, 0.1);
+    border-top: 4px solid #e2e2e2;
+  }
 
   @keyframes spin {
     0% {
@@ -21,8 +29,28 @@ const Loading = styled.div`
       transform: rotate(360deg);
     }
   }
+
+  @-webkit-keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+
+  @-moz-keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
 `;
 
-export const Loader: FC = () => {
-  return <Loading />;
+type Props = { className: string };
+
+export const Loader: FC<Props> = ({ className }: Props) => {
+  return <Loading className={className} />;
 };
