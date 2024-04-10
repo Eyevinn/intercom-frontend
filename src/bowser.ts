a3f8b28ff31ab3cf42e30ab503cad3dd51ecf103
malmen237 <linda.malm@eyevinn.se>
2024-04-10 12:20:57 +0200

    fix: added comment about avaliability and tablet-variable

--- src/bowser.ts
+++ src/bowser.ts
@@ -2,6 +2,9 @@ import Bowser from "bowser";
 
 const deviceInfo = Bowser.parse(window.navigator.userAgent);
 
+// platform type, can be either "desktop", "tablet" or "mobile"
 export const isMobile = deviceInfo.platform.type === "mobile";
+
+export const isTablet = deviceInfo.platform.type === "tablet";
 
 export const isDesktop = deviceInfo.platform.type === "desktop";
