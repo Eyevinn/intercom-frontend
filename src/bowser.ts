72707b24fe9551ecd9c117078ce7102aae4f4ae0
malmen237 <linda.malm@eyevinn.se>
2024-04-10 15:38:27 +0200

    fix: removed variables for desktop and tablet

--- src/bowser.ts
+++ src/bowser.ts
@@ -4,7 +4,3 @@ const deviceInfo = Bowser.parse(window.navigator.userAgent);
 
 // platform type, can be either "desktop", "tablet" or "mobile"
 export const isMobile = deviceInfo.platform.type === "mobile";
-
-export const isTablet = deviceInfo.platform.type === "tablet";
-
-export const isDesktop = deviceInfo.platform.type === "desktop";
