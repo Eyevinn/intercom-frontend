acaa63f611953319ed5b89d350cdddb1a12035b2
malmen237 <linda.malm@eyevinn.se>
2024-04-10 12:26:14 +0200

    fix: added test-log for tablet

--- src/components/landing-page/create-production.tsx
+++ src/components/landing-page/create-production.tsx
@@ -13,7 +13,7 @@ import {
 } from "./form-elements.tsx";
 import { API } from "../../api/api.ts";
 import { useGlobalState } from "../../global-state/context-provider.tsx";
-import { isDesktop, isMobile } from "../../bowser.ts";
+import { isDesktop, isMobile, isTablet } from "../../bowser.ts";
 
 type FormValues = {
   productionName: string;
@@ -67,6 +67,7 @@ export const CreateProduction = () => {
   useEffect(() => {
     // TODO remove this after testing
     console.log(`Is this a mobile? "${isMobile}"`);
+    console.log(`Is this a tablet? "${isTablet}"`);
     console.log(`Is this a desktop? "${isDesktop}"`);
     if (createdProductionId) {
       reset({
