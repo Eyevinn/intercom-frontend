849122fbaf84af5354ffafffbf2dcc98fa128459
malmen237 <linda.malm@eyevinn.se>
2024-04-11 15:47:06 +0200

    fix: changed svg-icons and removed unused css

--- src/assets/icons/icon.tsx
+++ src/assets/icons/icon.tsx
@@ -1,13 +1,6 @@
-import styled from "@emotion/styled";
-import MicMute from "./mic_off_24dp.svg";
-import MicUnmute from "./mic_on_24dp.svg";
+import MicMute from "./mic_off.svg";
+import MicUnmute from "./mic_on.svg";
 
-const Icon = styled.img`
-  width: 5rem;
-  padding-right: 1em;
-  pointer-events: none;
-`;
+export const MicMuted = () => <img src={MicMute} alt="off-microphone" />;
 
-export const MicMuted = () => <Icon src={MicMute} alt="red-microphone" />;
-
-export const MicUnmuted = () => <Icon src={MicUnmute} alt="green-microphone" />;
+export const MicUnmuted = () => <img src={MicUnmute} alt="on-microphone" />;
