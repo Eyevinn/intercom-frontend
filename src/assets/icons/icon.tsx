2dacae93d62f949e0e93c9c48f269256b10d9373
malmen237 <linda.malm@eyevinn.se>
2024-04-12 13:16:46 +0200

    feat: added delete-icon

--- src/assets/icons/icon.tsx
+++ src/assets/icons/icon.tsx
@@ -1,6 +1,7 @@
 import styled from "@emotion/styled";
 import MicMute from "./mic_off.svg";
 import MicUnmute from "./mic_on.svg";
+import RemoveIcon from "./remove_circle.svg";
 
 const Icon = styled.img`
   width: 100%;
@@ -8,6 +9,14 @@ const Icon = styled.img`
   display: block;
 `;
 
+const RemoveLineStyleing = styled.img`
+  width: 2.5rem;
+`;
+
 export const MicMuted = () => <Icon src={MicMute} alt="off-microphone" />;
 
 export const MicUnmuted = () => <Icon src={MicUnmute} alt="on-microphone" />;
+
+export const RemoveLine = () => (
+  <RemoveLineStyleing src={RemoveIcon} alt="delete-line" />
+);
