0dca3a7d7203dfa619f6fe00a1fc3211f2fa1846
malmen237 <linda.malm@eyevinn.se>
2024-04-16 12:45:11 +0200

    feat: added manage-button to landing-page

--- src/components/landing-page/landing-page.tsx
+++ src/components/landing-page/landing-page.tsx
@@ -1,13 +1,16 @@
+import { useNavigate } from "react-router-dom";
 import { JoinProduction } from "./join-production.tsx";
 import { CreateProduction } from "./create-production.tsx";
 import { ProductionsList } from "./productions-list.tsx";
 import { useNavigateToProduction } from "./use-navigate-to-production.ts";
 import { DisplayContainer, FlexContainer } from "../generic-components.ts";
 import { useGlobalState } from "../../global-state/context-provider.tsx";
+import { ActionButton } from "./form-elements.tsx";
 import { isMobile } from "../../bowser.ts";
 
 export const LandingPage = () => {
   const [{ joinProductionOptions }] = useGlobalState();
+  const navigate = useNavigate();
 
   useNavigateToProduction(joinProductionOptions);
 
@@ -24,6 +27,12 @@ export const LandingPage = () => {
         )}
       </FlexContainer>
       <ProductionsList />
+      <ActionButton
+        type="button"
+        onClick={() => navigate("/manage-productions")}
+      >
+        Manage Productions
+      </ActionButton>
     </>
   );
 };
