15c1a96404a14ddecce79d731ff9bafce406d988
Martin <901824+martinstark@users.noreply.github.com>
2024-04-16 12:53:15 +0200

    feat: update user list styling

--- src/components/production-line/user-list.tsx
+++ src/components/production-line/user-list.tsx
@@ -4,36 +4,55 @@ import { TParticipant } from "./types.ts";
 
 const ListWrapper = styled.div``;
 
-const User = styled.div`
+type TUserProps = {
+  isYou: boolean;
+  isActive: boolean;
+  isTalking: boolean;
+};
+
+const User = styled.div<TUserProps>`
   margin: 0 0 1rem;
   background: #1a1a1a;
   padding: 1rem;
   color: #ddd;
   max-width: 32rem;
-  display: flex;
-`;
+  border-radius: 0.5rem;
+  border: 0.1rem solid #1a1a1a;
 
-const GreenCircle = styled.div`
-  width: 1rem;
-  height: 1rem;
-  border-radius: 2rem;
-  display: inline-block;
-  background: #7be27b;
-  margin: 0 1rem 0 0;
+  ${({ isYou }) => (isYou ? `background: #353434;` : "")}
+
+  ${({ isActive }) =>
+    `border-left: 1rem solid ${isActive ? "#7be27b;" : "#ebca6a;"}`}
+
+  ${({ isTalking }) =>
+    isTalking
+      ? `
+  border-top: 0.1rem solid #7be27b;
+  border-bottom: 0.1rem solid #7be27b;
+  border-right: 0.1rem solid #7be27b;
+  `
+      : ""}
 `;
 
 type TUserListOptions = {
   participants: TParticipant[];
+  sessionid: string | null;
 };
-export const UserList = ({ participants }: TUserListOptions) => {
+export const UserList = ({ participants, sessionid }: TUserListOptions) => {
   if (!participants) return null;
 
   return (
     <ListWrapper>
       <DisplayContainerHeader>Participants</DisplayContainerHeader>
       {participants.map((p) => (
-        <User key={p.sessionid}>
-          <GreenCircle /> <div>{p.name}</div>
+        <User
+          key={p.sessionid}
+          isYou={p.sessionid === sessionid}
+          isActive={p.isActive}
+          // TODO connect to rtc data channel to get talking session id
+          isTalking={false}
+        >
+          {p.name} {p.isActive ? "" : "(inactive)"}
         </User>
       ))}
     </ListWrapper>
