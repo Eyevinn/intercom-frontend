5b4a44f5bd28287accc3deb58267fae3d15aae71
malmen237 <linda.malm@eyevinn.se>
2024-04-16 15:53:06 +0200

    fix: made clear-icon a generic size

--- src/assets/icons/icon.tsx
+++ src/assets/icons/icon.tsx
@@ -1,7 +1,7 @@
 import styled from "@emotion/styled";
 import MicMute from "./mic_off.svg";
 import MicUnmute from "./mic_on.svg";
-import RemoveIcon from "./clear.svg";
+import RemoveSvg from "./clear.svg";
 
 const Icon = styled.img`
   width: 100%;
@@ -9,14 +9,8 @@ const Icon = styled.img`
   display: block;
 `;
 
-const RemoveLineStyling = styled.img`
-  width: 2.5rem;
-`;
-
 export const MicMuted = () => <Icon src={MicMute} alt="off-microphone" />;
 
 export const MicUnmuted = () => <Icon src={MicUnmute} alt="on-microphone" />;
 
-export const RemoveLine = () => (
-  <RemoveLineStyling src={RemoveIcon} alt="cross" />
-);
+export const RemoveIcon = () => <Icon src={RemoveSvg} alt="cross" />;
