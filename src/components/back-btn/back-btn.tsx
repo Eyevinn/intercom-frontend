987e7206785ea7f1ae5af3dd44a80cd106766042
malmen237 <linda.malm@eyevinn.se>
2024-04-17 08:45:33 +0200

    feat: added a generic button that returns to previous page

---
+++ src/components/back-btn/back-btn.tsx
@@ -0,0 +1,21 @@
+import styled from "@emotion/styled";
+import { useNavigate } from "react-router-dom";
+import { BackArrow } from "../../assets/icons/icon";
+
+const StyledBackBtn = styled.button`
+  cursor: pointer;
+  padding: 0;
+  margin: 0;
+  background: transparent;
+  border: transparent;
+`;
+
+export const BackBtn = () => {
+  const navigate = useNavigate();
+
+  return (
+    <StyledBackBtn type="button" onClick={() => navigate(-1)}>
+      <BackArrow />
+    </StyledBackBtn>
+  );
+};
