89de3e9105be0b2260c80b5b71067eab488229cb
Martin <901824+martinstark@users.noreply.github.com>
2024-04-17 13:23:18 +0200

    chore: clean up production-line component

---
+++ src/components/production-line/use-check-bad-line-data.ts
@@ -0,0 +1,38 @@
+import { Dispatch, useEffect } from "react";
+import { TJoinProductionOptions } from "./types.ts";
+import { TGlobalStateAction } from "../../global-state/global-state-actions.ts";
+
+type TProps = {
+  joinProductionOptions: TJoinProductionOptions | null;
+  paramProductionId: string | undefined;
+  paramLineId: string | undefined;
+  dispatch: Dispatch<TGlobalStateAction>;
+};
+
+export const useCheckBadLineData = ({
+  joinProductionOptions,
+  paramProductionId,
+  paramLineId,
+  dispatch,
+}: TProps) => {
+  useEffect(() => {
+    if (!joinProductionOptions) {
+      const pidIsNan = Number.isNaN(
+        paramProductionId && parseInt(paramProductionId, 10)
+      );
+
+      const lidIsNan = Number.isNaN(paramLineId && parseInt(paramLineId, 10));
+
+      if (pidIsNan || lidIsNan) {
+        // Someone entered a production id in the URL that's not a number
+
+        const errorString = `Bad URL. ${pidIsNan ? "Production ID is not a number." : ""} ${lidIsNan ? "Line ID is not a number." : ""}`;
+
+        dispatch({
+          type: "ERROR",
+          payload: new Error(errorString),
+        });
+      }
+    }
+  }, [paramProductionId, paramLineId, joinProductionOptions, dispatch]);
+};
