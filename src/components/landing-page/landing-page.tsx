273fc53c9edc28cde2384c2c1af9dfb5943c99f2
malmen237 <linda.malm@eyevinn.se>
2024-04-18 08:34:43 +0200

    feat: added nav-button as a separate component, added styling

--- src/components/landing-page/landing-page.tsx
+++ src/components/landing-page/landing-page.tsx
@@ -1,16 +1,14 @@
-import { useNavigate } from "react-router-dom";
 import { JoinProduction } from "./join-production.tsx";
 import { CreateProduction } from "./create-production.tsx";
 import { ProductionsList } from "./productions-list.tsx";
 import { useNavigateToProduction } from "./use-navigate-to-production.ts";
 import { DisplayContainer, FlexContainer } from "../generic-components.ts";
 import { useGlobalState } from "../../global-state/context-provider.tsx";
-import { ActionButton } from "./form-elements.tsx";
 import { isMobile } from "../../bowser.ts";
+import { ManageProductionButton } from "../manage-productions/manage-production-button.tsx";
 
 export const LandingPage = () => {
   const [{ joinProductionOptions }] = useGlobalState();
-  const navigate = useNavigate();
 
   useNavigateToProduction(joinProductionOptions);
 
@@ -27,12 +25,7 @@ export const LandingPage = () => {
         )}
       </FlexContainer>
       <ProductionsList />
-      <ActionButton
-        type="button"
-        onClick={() => navigate("/manage-productions")}
-      >
-        Manage Productions
-      </ActionButton>
+      <ManageProductionButton />
     </>
   );
 };
