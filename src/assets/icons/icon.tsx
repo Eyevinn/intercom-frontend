da0d7d8a980a4ef2dc047ce43faaae11d14868b3
malmen237 <linda.malm@eyevinn.se>
2024-04-18 15:30:36 +0200

    feat: added icons for speaker on and off

--- src/assets/icons/icon.tsx
+++ src/assets/icons/icon.tsx
@@ -2,6 +2,8 @@ import styled from "@emotion/styled";
 import MicMute from "./mic_off.svg";
 import MicUnmute from "./mic_on.svg";
 import RemoveSvg from "./clear.svg";
+import VolumeOn from "./volume_on.svg";
+import VolumeOff from "./volume_off.svg";
 
 const Icon = styled.img`
   width: 100%;
@@ -14,3 +16,9 @@ export const MicMuted = () => <Icon src={MicMute} alt="off-microphone" />;
 export const MicUnmuted = () => <Icon src={MicUnmute} alt="on-microphone" />;
 
 export const RemoveIcon = () => <Icon src={RemoveSvg} alt="cross" />;
+
+export const SpeakerOff = () => (
+  <Icon src={VolumeOff} alt="speaker-crossed-over" />
+);
+
+export const SpeakerOn = () => <Icon src={VolumeOn} alt="speaker" />;
