1f8d855244fd4e79ef18321236fc8ff7895ad09c
malmen237 <linda.malm@eyevinn.se>
2024-04-19 14:23:52 +0200

    feat: created a delete-production-hook

---
+++ src/components/manage-productions/use-delete-production.ts
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+import { API } from "../../api/api";
+import { useGlobalState } from "../../global-state/context-provider";
+
+type TUseDeleteProduction = (id: number | null) => {
+  loading: boolean;
+  error: Error | null;
+  successfullDelete: boolean;
+};
+
+export const useDeleteProduction: TUseDeleteProduction = (id) => {
+  const [successfullDelete, setSuccessfullDelete] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const [, dispatch] = useGlobalState();
+
+  useEffect(() => {
+    let aborted = false;
+    setError(null);
+    setSuccessfullDelete(false);
+    setLoading(true);
+    if (id) {
+      API.deleteProduction(id)
+        .then(() => {
+          if (aborted) return;
+
+          setSuccessfullDelete(true);
+          setLoading(false);
+          setError(null);
+        })
+        .catch((err) => {
+          dispatch({
+            type: "ERROR",
+            payload:
+              err instanceof Error
+                ? err
+                : new Error("Failed to delete production"),
+          });
+          setError(err);
+          setLoading(false);
+        });
+    } else {
+      setLoading(false);
+    }
+
+    return () => {
+      aborted = true;
+    };
+  }, [dispatch, id]);
+
+  return {
+    loading,
+    error,
+    successfullDelete,
+  };
+};
