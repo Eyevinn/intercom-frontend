38d66c0d5c7bd015281d48119f17609566c0e3d4
malmen237 <linda.malm@eyevinn.se>
2024-04-24 13:37:56 +0200

    feat: added component form-input-with-loader

---
+++ src/components/landing-page/form-input-with-loader.tsx
@@ -0,0 +1,49 @@
+import { ChangeHandler, RefCallBack } from "react-hook-form";
+import styled from "@emotion/styled";
+import { Spinner } from "../loader/loader";
+import { FormLabel, DecorativeLabel, FormInput } from "./form-elements";
+
+type TFormInputWithLoader = {
+  label: string;
+  placeholder: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  name: string;
+  ref: RefCallBack;
+  onBlur: ChangeHandler;
+  type: string;
+  loading: boolean;
+};
+
+const InputWrapper = styled.div`
+  position: relative;
+`;
+
+export const FormInputWithLoader = ({
+  label,
+  placeholder,
+  onChange,
+  name,
+  ref,
+  onBlur,
+  type,
+  loading,
+}: TFormInputWithLoader) => {
+  return (
+    <FormLabel>
+      <DecorativeLabel>{label}</DecorativeLabel>
+      <InputWrapper>
+        <FormInput
+          className="with-loader"
+          onChange={onChange}
+          name={name}
+          ref={ref}
+          onBlur={onBlur}
+          type={type}
+          placeholder={placeholder}
+          autoComplete="off"
+        />
+        {loading && <Spinner className="form-loader" />}
+      </InputWrapper>
+    </FormLabel>
+  );
+};
