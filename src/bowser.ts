6ce3dc3d537f25397a2e8355c6491b866865551f
malmen237 <linda.malm@eyevinn.se>
2024-04-30 15:33:19 +0200

    feat: added browser-name and version as bowser-variables

--- src/bowser.ts
+++ src/bowser.ts
@@ -4,3 +4,12 @@ const deviceInfo = Bowser.parse(window.navigator.userAgent);
 
 // platform type, can be either "desktop", "tablet" or "mobile"
 export const isMobile = deviceInfo.platform.type === "mobile";
+
+export const browserName = deviceInfo.browser.name;
+
+export const browserVersion = deviceInfo.browser.version
+  ? deviceInfo.browser.version
+  : "";
+
+// TODO remove when not needed anymore
+console.log("List of all BOWSER-supported browsers", Bowser.BROWSER_MAP);
