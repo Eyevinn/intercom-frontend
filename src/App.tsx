661086a222ae02fbe2ec708004d4f2db868f9c65
malmen237 <linda.malm@eyevinn.se>
2024-04-30 15:39:27 +0200

    feat: added warning before media-permission-fetch if user has unsupported browser or version

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import styled from "@emotion/styled";
+import { useState } from "react";
 import { ProductionLine } from "./components/production-line/production-line.tsx";
 import { ErrorPage } from "./components/router-error.tsx";
 import { useDevicePermissions } from "./use-device-permission.ts";
@@ -15,6 +16,7 @@ import {
 } from "./components/generic-components.ts";
 import { DisplayWarning } from "./components/display-box.tsx";
 import { ManageProductions } from "./components/manage-productions/manage-productions.tsx";
+import { useCheckForSupportedBrowser } from "./use-check-for-supported-browser.ts";
 import { DisplayContainerHeader } from "./components/landing-page/display-container-header.tsx";
 import { NavigateToRootButton } from "./components/navigate-to-root-button/navigate-to-root-button.tsx";
 
@@ -43,9 +45,14 @@ const NotFound = () => {
 };
 
 const App = () => {
+  const { browserSupported, browserUpdated } = useCheckForSupportedBrowser();
   const { denied, permission } = useDevicePermissions();
   const initializedGlobalState = useInitializeGlobalStateReducer();
   const [, dispatch] = initializedGlobalState;
+  const [unsupportedContinue, setUnsupportedContinue] = useState(false);
+
+  const continueToApp =
+    (browserSupported && browserUpdated) || unsupportedContinue;
 
   useFetchDevices({
     dispatch,
@@ -57,46 +64,67 @@ const App = () => {
       <Header />
       <ErrorBanner />
 
-      {denied && (
+      {!browserSupported && !unsupportedContinue && (
         <DisplayBoxPositioningContainer>
           <DisplayWarning
-            text="To use this application it has to be granted access to audio devices. Reload browser and/or reset permissions to try
+            text="To use this application it is recommended to use one of the following browsers: Chrome, Windows Edge, Firefox or Safari."
+            title="Browser not supported"
+            btn={() => setUnsupportedContinue(true)}
+          />
+        </DisplayBoxPositioningContainer>
+      )}
+
+      {browserSupported && !browserUpdated && !unsupportedContinue && (
+        <DisplayBoxPositioningContainer>
+          <DisplayWarning
+            text="To use this application we recommend using a newer version of your browser, please update and try again."
+            title="Browser to old"
+            btn={() => setUnsupportedContinue(true)}
+          />
+        </DisplayBoxPositioningContainer>
+      )}
+      {continueToApp && (
+        <>
+          {denied && (
+            <DisplayBoxPositioningContainer>
+              <DisplayWarning
+                text="To use this application it has to be granted access to audio devices. Reload browser and/or reset permissions to try
             again."
-            title="Permissions have been denied"
-          />
-        </DisplayBoxPositioningContainer>
-      )}
-
-      {!permission && !denied && (
-        <DisplayBoxPositioningContainer>
-          <DisplayWarning
-            text="To use this application it has to be granted access to audio devices."
-            title="Waiting for device permissions"
-          />
-        </DisplayBoxPositioningContainer>
-      )}
-
-      {permission && !denied && (
-        <BrowserRouter>
-          <Routes>
-            <Route
-              path="/"
-              element={<LandingPage />}
-              errorElement={<ErrorPage />}
-            />
-            <Route
-              path="/manage-productions"
-              element={<ManageProductions />}
-              errorElement={<ErrorPage />}
-            />
-            <Route
-              path="/production/:productionId/line/:lineId"
-              element={<ProductionLine />}
-              errorElement={<ErrorPage />}
-            />
+                title="Permissions have been denied"
+              />
+            </DisplayBoxPositioningContainer>
+          )}
+          {!permission && !denied && (
+            <DisplayBoxPositioningContainer>
+              <DisplayWarning
+                text="To use this application it has to be granted access to audio devices."
+                title="Waiting for device permissions"
+              />
+            </DisplayBoxPositioningContainer>
+          )}
+          {permission && !denied && (
+            <BrowserRouter>
+              <Routes>
+                <Route
+                  path="/"
+                  element={<LandingPage />}
+                  errorElement={<ErrorPage />}
+                />
+                <Route
+                  path="/manage-productions"
+                  element={<ManageProductions />}
+                  errorElement={<ErrorPage />}
+                />
+                <Route
+                  path="/production/:productionId/line/:lineId"
+                  element={<ProductionLine />}
+                  errorElement={<ErrorPage />}
+                />
             <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
+              </Routes>
+            </BrowserRouter>
+          )}
+        </>
       )}
     </GlobalStateContext.Provider>
   );
