0ad121ef95f1174e3fcf66114c010e26f397f9f8
malmen237 <linda.malm@eyevinn.se>
2024-05-02 11:50:09 +0200

    feat: added navigate-to-root-btn to header and added styling to header-text

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,35 @@
 import styled from "@emotion/styled";
 import { FC } from "react";
 import { backgroundColour } from "../css-helpers/defaults.ts";
+import { NavigateToRootButton } from "./navigate-to-root-button/navigate-to-root-button.tsx";
 
 const HeaderWrapper = styled.div`
   width: 100%;
   background: ${backgroundColour};
   padding: 1rem;
-  font-size: 2rem;
-  font-weight: bold;
   margin: 0 0 1rem 0;
 `;
 
+const HeaderText = styled.p`
+  font-size: 2rem;
+  font-weight: bold;
+  font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+  color-scheme: light dark;
+  color: rgba(255, 255, 255, 0.87);
+  background: ${backgroundColour};
+
+  font-synthesis: none;
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+`;
+
 export const Header: FC = () => {
-  return <HeaderWrapper>Intercom</HeaderWrapper>;
+  return (
+    <HeaderWrapper>
+      <NavigateToRootButton header resetOnExit>
+        <HeaderText>Intercom</HeaderText>
+      </NavigateToRootButton>
+    </HeaderWrapper>
+  );
 };
