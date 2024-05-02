9e80d1b6d3a57baf142b3a5c369e10dad8199f66
malmen237 <linda.malm@eyevinn.se>
2024-05-02 16:56:41 +0200

    feat: added reset and navigation to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,35 +1,38 @@
 import styled from "@emotion/styled";
 import { FC } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import { backgroundColour } from "../css-helpers/defaults.ts";
-import { NavigateToRootButton } from "./navigate-to-root-button/navigate-to-root-button.tsx";
+import { useGlobalState } from "../global-state/context-provider.tsx";
 
 const HeaderWrapper = styled.div`
   width: 100%;
   background: ${backgroundColour};
   padding: 1rem;
-  margin: 0 0 1rem 0;
-`;
-
-const HeaderText = styled.p`
   font-size: 2rem;
   font-weight: bold;
-  font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
-  color-scheme: light dark;
-  color: rgba(255, 255, 255, 0.87);
-  background: ${backgroundColour};
-
-  font-synthesis: none;
-  text-rendering: optimizeLegibility;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  margin: 0 0 1rem 0;
+  cursor: pointer;
 `;
 
 export const Header: FC = () => {
-  return (
-    <HeaderWrapper>
-      <NavigateToRootButton header resetOnExit>
-        <HeaderText>Intercom</HeaderText>
-      </NavigateToRootButton>
-    </HeaderWrapper>
-  );
+  const [, dispatch] = useGlobalState();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const reset = () => {
+    dispatch({
+      type: "UPDATE_JOIN_PRODUCTION_OPTIONS",
+      payload: null,
+    });
+  };
+
+  const returnToRoot = () => {
+    if (location.pathname.includes("/production")) {
+      reset();
+      navigate("/");
+    } else {
+      navigate("/");
+    }
+  };
+  return <HeaderWrapper onClick={() => returnToRoot()}>Intercom</HeaderWrapper>;
 };
