a1f19ea636f6225b62f8861fabbbbfbfd32a9b27
malmen237 <linda.malm@eyevinn.se>
2024-05-02 16:55:55 +0200

    fix: removed reset-functionality and header from nav-to-root-btn

--- src/components/navigate-to-root-button/navigate-to-root-button.tsx
+++ src/components/navigate-to-root-button/navigate-to-root-button.tsx
@@ -2,7 +2,6 @@ import styled from "@emotion/styled";
 import { useNavigate } from "react-router-dom";
 import { BackArrow } from "../../assets/icons/icon";
 import { PrimaryButton } from "../landing-page/form-elements";
-import { useGlobalState } from "../../global-state/context-provider";
 
 const StyledBackBtn = styled(PrimaryButton)`
   padding: 0;
@@ -11,35 +10,23 @@ const StyledBackBtn = styled(PrimaryButton)`
 `;
 
 export const NavigateToRootButton = ({
-  children,
   resetOnExit,
-  header,
 }: {
-  children?: React.ReactNode;
-  resetOnExit?: boolean;
-  header?: boolean;
+  resetOnExit?: () => void;
 }) => {
-  const [, dispatch] = useGlobalState();
   const navigate = useNavigate();
-
-  const reset = () => {
-    dispatch({
-      type: "UPDATE_JOIN_PRODUCTION_OPTIONS",
-      payload: null,
-    });
-  };
 
   return (
     <StyledBackBtn
       type="button"
       onClick={() => {
         if (resetOnExit) {
-          reset();
+          resetOnExit();
         }
         navigate("/");
       }}
     >
-      {!header ? <BackArrow /> : children}
+      <BackArrow />
     </StyledBackBtn>
   );
 };
