d7f46e528dd6de8d85cfd05b1ab988f15dac7712
malmen237 <linda.malm@eyevinn.se>
2024-05-06 14:57:48 +0200

    fix: added popstate-eventlistener and exit-reset to nav-to-root-btn

--- src/components/navigate-to-root-button/navigate-to-root-button.tsx
+++ src/components/navigate-to-root-button/navigate-to-root-button.tsx
@@ -1,7 +1,9 @@
 import styled from "@emotion/styled";
 import { useNavigate } from "react-router-dom";
+import { useCallback, useEffect } from "react";
 import { BackArrow } from "../../assets/icons/icon";
 import { PrimaryButton } from "../landing-page/form-elements";
+import { useGlobalState } from "../../global-state/context-provider";
 
 const StyledBackBtn = styled(PrimaryButton)`
   padding: 0;
@@ -12,18 +14,33 @@ const StyledBackBtn = styled(PrimaryButton)`
 export const NavigateToRootButton = ({
   resetOnExit,
 }: {
-  resetOnExit?: () => void;
+  resetOnExit?: boolean;
 }) => {
   const navigate = useNavigate();
+  const [, dispatch] = useGlobalState();
+
+  const exit = useCallback(() => {
+    navigate("/");
+    dispatch({
+      type: "UPDATE_JOIN_PRODUCTION_OPTIONS",
+      payload: null,
+    });
+    window.removeEventListener("popstate", exit);
+  }, [dispatch, navigate]);
+
+  useEffect(() => {
+    window.addEventListener("popstate", exit);
+  }, [exit]);
 
   return (
     <StyledBackBtn
       type="button"
       onClick={() => {
         if (resetOnExit) {
-          resetOnExit();
+          exit();
+        } else {
+          navigate("/");
         }
-        navigate("/");
       }}
     >
       <BackArrow />
