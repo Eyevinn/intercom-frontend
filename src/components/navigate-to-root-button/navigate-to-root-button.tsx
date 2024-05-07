797f011ea6f5852314413005f20e12ba676d1c20
malmen237 <linda.malm@eyevinn.se>
2024-05-07 15:14:49 +0200

    fix: remove cleanup-logic from button

--- src/components/navigate-to-root-button/navigate-to-root-button.tsx
+++ src/components/navigate-to-root-button/navigate-to-root-button.tsx
@@ -1,9 +1,7 @@
 import styled from "@emotion/styled";
 import { useNavigate } from "react-router-dom";
-import { useCallback, useEffect } from "react";
 import { BackArrow } from "../../assets/icons/icon";
 import { PrimaryButton } from "../landing-page/form-elements";
-import { useGlobalState } from "../../global-state/context-provider";
 
 const StyledBackBtn = styled(PrimaryButton)`
   padding: 0;
@@ -14,30 +12,16 @@ const StyledBackBtn = styled(PrimaryButton)`
 export const NavigateToRootButton = ({
   resetOnExit,
 }: {
-  resetOnExit?: boolean;
+  resetOnExit?: () => void;
 }) => {
   const navigate = useNavigate();
-  const [, dispatch] = useGlobalState();
-
-  const exit = useCallback(() => {
-    navigate("/");
-    dispatch({
-      type: "UPDATE_JOIN_PRODUCTION_OPTIONS",
-      payload: null,
-    });
-    window.removeEventListener("popstate", exit);
-  }, [dispatch, navigate]);
-
-  useEffect(() => {
-    window.addEventListener("popstate", exit);
-  }, [exit]);
 
   return (
     <StyledBackBtn
       type="button"
       onClick={() => {
         if (resetOnExit) {
-          exit();
+          resetOnExit();
         } else {
           navigate("/");
         }
