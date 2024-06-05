bf147be23503a9dd92b62ee63980e63c3005208f
Jonas Birmé <jonas.birme@eyevinn.se>
2024-06-05 13:23:11 +0200

    fix: default to port 3000 if env PORT not provided

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sed -i "s/listen\s*8080;/listen $PORT;/" /etc/nginx/conf.d/default.conf
+LISTENPORT="${PORT:-3000}"
+sed -i "s/listen\s*8080;/listen $LISTENPORT;/" /etc/nginx/conf.d/default.conf
 
 VITE_BACKEND_URL=$MANAGER_URL yarn build && \
   cp -r /app/dist/* /usr/share/nginx/html/ && \
