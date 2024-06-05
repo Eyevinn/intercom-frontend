2cfddb0cd80d72d4b41bf0dcc505960a32177d6e
Jonas Birmé <jonas.birme@eyevinn.se>
2024-06-05 16:59:16 +0200

    fix: default manager url

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,7 +2,8 @@
 
 LISTENPORT="${PORT:-3000}"
 sed -i "s/listen\s*8080;/listen $LISTENPORT;/" /etc/nginx/conf.d/default.conf
+API_URL="${MANAGER_URL:-/}"
 
-VITE_BACKEND_URL=$MANAGER_URL yarn build && \
+VITE_BACKEND_URL=$API_URL yarn build && \
   cp -r /app/dist/* /usr/share/nginx/html/ && \
   nginx -g 'daemon off;'
