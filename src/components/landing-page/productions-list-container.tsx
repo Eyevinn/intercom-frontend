195f16b5d0ae37a2d60bc340ab9b99a1f6136311
malmen237 <linda.malm@eyevinn.se>
2024-06-28 18:11:33 +0200

    fix: renamed production-list to production-list-container and separated list into its own component

--- src/components/landing-page/productions-list-container.tsx
+++ src/components/landing-page/productions-list-container.tsx
@@ -6,41 +6,24 @@ import { useRefreshAnimation } from "./use-refresh-animation.ts";
 import { DisplayContainerHeader } from "./display-container-header.tsx";
 import { DisplayContainer } from "../generic-components.ts";
 import { ManageProductionButton } from "./manage-production-button.tsx";
-import { LocalError } from "../error.tsx";
 import { useFetchProductionList } from "./use-fetch-production-list.ts";
-
-const ProductionListContainer = styled.div`
-  display: flex;
-  padding: 0 0 2rem 2rem;
-  flex-wrap: wrap;
-`;
+import { ProductionsList } from "../productions-list.tsx";
 
 const HeaderContainer = styled(DisplayContainer)`
   padding: 0 2rem 0 2rem;
 `;
 
-const ProductionItem = styled.div`
-  flex: 1 0 calc(25% - 2rem);
-  min-width: 20rem;
-  border: 1px solid #424242;
-  border-radius: 0.5rem;
-  padding: 2rem;
-  margin: 0 2rem 2rem 0;
+const LoaderWrapper = styled.div`
+  padding-bottom: 1rem;
 `;
 
-const ProductionName = styled.div`
-  font-size: 1.4rem;
-  font-weight: bold;
-  margin: 0 0 1rem;
-  word-break: break-word;
+const ListWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  padding: 0 0 0 2rem;
 `;
 
-const ProductionId = styled.div`
-  font-size: 2rem;
-  color: #9e9e9e;
-`;
-
-export const ProductionsList = () => {
+export const ProductionsListContainer = () => {
   const [{ reloadProductionList }] = useGlobalState();
 
   const { productions, doInitialLoad, error, setIntervalLoad } =
@@ -63,23 +46,18 @@ export const ProductionsList = () => {
 
   return (
     <>
-      <LoaderDots
-        className={showRefreshing ? "active" : "in-active"}
-        text="refreshing"
-      />
+      <LoaderWrapper>
+        <LoaderDots
+          className={showRefreshing ? "active" : "in-active"}
+          text="refreshing"
+        />
+      </LoaderWrapper>
       <HeaderContainer>
         <DisplayContainerHeader>Production List</DisplayContainerHeader>
       </HeaderContainer>
-      <ProductionListContainer>
-        {error && <LocalError error={error} />}
-        {!error &&
-          productions.map((p) => (
-            <ProductionItem key={p.productionId}>
-              <ProductionName>{p.name}</ProductionName>
-              <ProductionId>{p.productionId}</ProductionId>
-            </ProductionItem>
-          ))}
-      </ProductionListContainer>
+      <ListWrapper>
+        <ProductionsList productions={productions} error={error} />
+      </ListWrapper>
       {!!productions.length && <ManageProductionButton />}
     </>
   );
