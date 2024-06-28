9ab3c4da014efa85c276a3e46717d357a3eb6ef1
malmen237 <linda.malm@eyevinn.se>
2024-06-28 18:15:26 +0200

    feat: added list of productions and a show-list-btn, rearranged some error-messages and messangers

--- src/components/manage-productions/manage-productions.tsx
+++ src/components/manage-productions/manage-productions.tsx
@@ -5,6 +5,7 @@ import styled from "@emotion/styled";
 import { DisplayContainerHeader } from "../landing-page/display-container-header";
 import {
   DecorativeLabel,
+  PrimaryButton,
   StyledWarningMessage,
 } from "../landing-page/form-elements";
 import { useFetchProduction } from "../landing-page/use-fetch-production";
@@ -15,10 +16,20 @@ import { FormInputWithLoader } from "../landing-page/form-input-with-loader";
 import { RemoveProduction } from "./remove-production";
 import { ManageLines } from "./manage-lines";
 import { TProduction } from "../production-line/types";
+import { ProductionsList } from "../productions-list";
+import { useFetchProductionList } from "../landing-page/use-fetch-production-list";
 
 type FormValue = {
   productionId: string;
 };
+
+const ShowListBtn = styled(PrimaryButton)`
+  margin-bottom: 2rem;
+`;
+
+const FormInputWrapper = styled.div`
+  max-width: 45rem;
+`;
 
 const SubContainers = styled.div`
   width: 100%;
@@ -28,8 +39,13 @@ const SubContainers = styled.div`
 `;
 
 const Container = styled.form`
-  max-width: 45rem;
   padding: 1rem 2rem 0 2rem;
+`;
+
+const ListWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
 `;
 
 const RemoveConfirmation = styled.div`
@@ -55,6 +71,8 @@ export const ManageProductions = () => {
   const [showDeleteDoneMessage, setShowDeleteDoneMessage] =
     useState<boolean>(false);
   const [verifyRemove, setVerifyRemove] = useState<boolean>(false);
+  const [showProductionsList, setShowProductionsList] =
+    useState<boolean>(false);
   const [delayOnGuideText, setDelayOnGuideText] = useState<boolean>(false);
   const [removeId, setRemoveId] = useState<null | number>(null);
   const [cachedProduction, setCachedProduction] = useState<null | TProduction>(
@@ -77,6 +95,8 @@ export const ManageProductions = () => {
     required: "Production ID is required",
     min: 1,
   });
+
+  const { productions, error } = useFetchProductionList();
 
   const {
     error: productionFetchError,
@@ -110,6 +130,7 @@ export const ManageProductions = () => {
     if (production) {
       setCachedProduction(production);
       setProductionIdToFetch(null);
+      setShowProductionsList(false);
     }
   }, [production]);
 
@@ -159,35 +180,61 @@ export const ManageProductions = () => {
         <NavigateToRootButton />
       </StyledBackBtnIcon>
       <DisplayContainerHeader>Manage Productions</DisplayContainerHeader>
-      <FormInputWithLoader
-        onChange={(ev) => {
-          onChange(ev);
-          const pid = parseInt(ev.target.value, 10);
-          const confirmedPid = Number.isNaN(pid) ? null : pid;
+      {cachedProduction && !showProductionsList && (
+        <ShowListBtn
+          type="button"
+          onClick={() => {
+            setShowProductionsList(true);
+          }}
+        >
+          Show Productions List
+        </ShowListBtn>
+      )}
+      {(!cachedProduction || showProductionsList) && (
+        <ListWrapper>
+          <ProductionsList
+            productions={productions}
+            error={error}
+            manageProduction={(v: string) => {
+              setProductionIdToFetch(parseInt(v, 10));
+              reset({
+                productionId: `${v}`,
+              });
+              setShowProductionsList(false);
+            }}
+          />
+        </ListWrapper>
+      )}
+      <FormInputWrapper>
+        <FormInputWithLoader
+          onChange={(ev) => {
+            onChange(ev);
+            const pid = parseInt(ev.target.value, 10);
+            const confirmedPid = Number.isNaN(pid) ? null : pid;
 
-          setProductionIdToFetch(confirmedPid);
-          setShowDeleteDoneMessage(false);
-        }}
-        label="Production ID"
-        placeholder="Production ID"
-        name={name}
-        inputRef={ref}
-        onBlur={onBlur}
-        type="number"
-        loading={fetchLoader}
-      />
-      {productionFetchError && (
-        <FetchErrorMessage>
-          The production ID could not be fetched. {productionFetchError.name}{" "}
-          {productionFetchError.message}.
-        </FetchErrorMessage>
-      )}
-      {productionDeleteError && (
-        <FetchErrorMessage>
-          The production ID could not be deleted. {productionDeleteError.name}{" "}
-          {productionDeleteError.message}.
-        </FetchErrorMessage>
-      )}
+            setProductionIdToFetch(confirmedPid);
+            setShowDeleteDoneMessage(false);
+          }}
+          label="Production ID"
+          placeholder="Production ID"
+          name={name}
+          inputRef={ref}
+          onBlur={onBlur}
+          type="number"
+          loading={fetchLoader}
+        />
+        {delayOnGuideText && (
+          <StyledWarningMessage>
+            Please enter a production id
+          </StyledWarningMessage>
+        )}
+        {productionFetchError && (
+          <FetchErrorMessage>
+            The production ID could not be fetched. {productionFetchError.name}{" "}
+            {productionFetchError.message}.
+          </FetchErrorMessage>
+        )}
+      </FormInputWrapper>
       <ErrorMessage
         errors={errors}
         name="productionId"
@@ -211,15 +258,20 @@ export const ManageProductions = () => {
               reset={() => {
                 setVerifyRemove(false);
                 setRemoveId(null);
+                setCachedProduction(null);
+                reset({
+                  productionId: "",
+                });
               }}
             />
           </SubContainers>
         </>
       )}
-      {delayOnGuideText && (
-        <StyledWarningMessage>
-          Please enter a production id
-        </StyledWarningMessage>
+      {productionDeleteError && (
+        <FetchErrorMessage>
+          The production ID could not be deleted. {productionDeleteError.name}{" "}
+          {productionDeleteError.message}.
+        </FetchErrorMessage>
       )}
       {showDeleteDoneMessage && (
         <RemoveConfirmation>
