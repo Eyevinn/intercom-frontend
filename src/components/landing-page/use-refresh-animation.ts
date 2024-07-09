f6cbd9f2f10042e60a62981fbd6446cfe59f4734
malmen237 <linda.malm@eyevinn.se>
2024-07-09 08:20:51 +0200

    fix: made reload-prop optional

--- src/components/landing-page/use-refresh-animation.ts
+++ src/components/landing-page/use-refresh-animation.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 type TUseRefreshAnimationOptions = {
-  reloadProductionList: boolean;
+  reloadProductionList?: boolean;
   doInitialLoad: boolean;
 };
 
