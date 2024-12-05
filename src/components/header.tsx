afbe705efb441b752f5741b30ab6953b02409b85
malmen237 <linda.malm@eyevinn.se>
2024-12-05 13:29:21 +0100

    feat: added confirmation-modal to all leave-call-buttons

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,13 @@
 import styled from "@emotion/styled";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { backgroundColour } from "../css-helpers/defaults.ts";
 import { useGlobalState } from "../global-state/context-provider.tsx";
 import { useAudioCue } from "./production-line/use-audio-cue.ts";
+import { DisplayContainerHeader } from "./landing-page/display-container-header.tsx";
+import { Modal } from "./modal/modal.tsx";
+import { VerifyDecision } from "./verify-decision/verify-decision.tsx";
+import { ModalConfirmationText } from "./modal/modal-confirmation-text.ts";
 
 const HeaderWrapper = styled.div`
   width: 100%;
@@ -16,12 +20,14 @@ const HeaderWrapper = styled.div`
 `;
 
 export const Header: FC = () => {
+  const [confirmExitModalOpen, setConfirmExitModalOpen] = useState(false);
   const [{ calls }, dispatch] = useGlobalState();
   const navigate = useNavigate();
   const location = useLocation();
   const { playExitSound } = useAudioCue();
 
   const runExitAllCalls = () => {
+    navigate("/");
     playExitSound();
     Object.entries(calls).forEach(([callId]) => {
       if (callId) {
@@ -35,11 +41,26 @@ export const Header: FC = () => {
 
   const returnToRoot = () => {
     if (location.pathname.includes("/production")) {
-      runExitAllCalls();
-      navigate("/");
+      setConfirmExitModalOpen(true);
     } else {
       navigate("/");
     }
   };
-  return <HeaderWrapper onClick={returnToRoot}>Intercom</HeaderWrapper>;
+  return (
+    <>
+      <HeaderWrapper onClick={returnToRoot}>Intercom</HeaderWrapper>
+      {confirmExitModalOpen && (
+        <Modal onClose={() => setConfirmExitModalOpen(false)}>
+          <DisplayContainerHeader>Confirm</DisplayContainerHeader>
+          <ModalConfirmationText>
+            Are you sure you want to leave all calls?
+          </ModalConfirmationText>
+          <VerifyDecision
+            confirm={runExitAllCalls}
+            abort={() => setConfirmExitModalOpen(false)}
+          />
+        </Modal>
+      )}
+    </>
+  );
 };
