6af4a9c354b54ffc08005c1d3998ec97c90d1222
malmen237 <linda.malm@eyevinn.se>
2024-12-10 11:18:18 +0100

    fix: moved the navigation-hook-call from the landing-page to the join-production-component

--- src/components/landing-page/landing-page.tsx
+++ src/components/landing-page/landing-page.tsx
@@ -1,33 +1,19 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { JoinProduction } from "./join-production.tsx";
 import { CreateProduction } from "./create-production.tsx";
 import { ProductionsListContainer } from "./productions-list-container.tsx";
-import { useNavigateToProduction } from "./use-navigate-to-production.ts";
 import { DisplayContainer, FlexContainer } from "../generic-components.ts";
 import { useGlobalState } from "../../global-state/context-provider.tsx";
 import { isMobile } from "../../bowser.ts";
-import { TJoinProductionOptions } from "../production-line/types.ts";
 
 export const LandingPage = ({ setApiError }: { setApiError: () => void }) => {
-  const [singleJoinProductionOptions, setSingleJoinProductionOptions] =
-    useState<TJoinProductionOptions | null>(null);
-  const [{ calls, apiError }] = useGlobalState();
-
-  useEffect(() => {
-    Object.entries(calls).forEach(([callId, callState]) => {
-      if (callId) {
-        setSingleJoinProductionOptions(callState.joinProductionOptions);
-      }
-    });
-  }, [calls]);
+  const [{ apiError }] = useGlobalState();
 
   useEffect(() => {
     if (apiError) {
       setApiError();
     }
   }, [apiError, setApiError]);
-
-  useNavigateToProduction(singleJoinProductionOptions);
 
   return (
     <>
