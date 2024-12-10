82879c885de1b4384a69ae5ccfe7c5af5af8effc
malmen237 <linda.malm@eyevinn.se>
2024-12-10 11:00:08 +0100

    fix: added reset to confirmation-modal on header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -37,6 +37,7 @@ export const Header: FC = () => {
   const { playExitSound } = useAudioCue();
 
   const runExitAllCalls = () => {
+    setConfirmExitModalOpen(false);
     navigate("/");
     playExitSound();
     Object.entries(calls).forEach(([callId]) => {
