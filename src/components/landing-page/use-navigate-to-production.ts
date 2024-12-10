a2ba53162f2f3b709e376c96efe34e6b4e5fda56
malmen237 <linda.malm@eyevinn.se>
2024-12-10 11:19:22 +0100

    fix: removed old solution left during testing

--- src/components/landing-page/use-navigate-to-production.ts
+++ src/components/landing-page/use-navigate-to-production.ts
@@ -1,23 +1,12 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { TJoinProductionOptions } from "../production-line/types.ts";
-// import { isMobile } from "../../bowser.ts";
 
 // Navigates to a production line as soon as a new production is pushed to the global state
 export const useNavigateToProduction = (
   joinProductionOptions: TJoinProductionOptions | null
 ) => {
   const navigate = useNavigate();
-
-  // useEffect(() => {
-  //   if (joinProductionOptions && !isMobile) {
-  //     navigate("/production-calls");
-  //   } else if (joinProductionOptions && isMobile) {
-  //     navigate(
-  //       `/production-calls/production/${joinProductionOptions.productionId}/line/${joinProductionOptions.lineId}`
-  //     );
-  //   }
-  // }, [navigate, joinProductionOptions]);
 
   useEffect(() => {
     if (joinProductionOptions) {
