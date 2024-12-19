2ccd8217cfc9b116c3c2fd4d815df428281f3dd3
Saelmala <sandra.larsson@eyevinn.se>
2024-12-19 13:22:29 +0100

    fix: dont show volume controls on tablet

--- src/bowser.ts
+++ src/bowser.ts
@@ -7,6 +7,8 @@ const browserName = browser.getBrowserName();
 // platform type, can be either "desktop", "tablet" or "mobile"
 export const isMobile = deviceInfo.platform.type === "mobile";
 
+export const isTablet = deviceInfo.platform.type === "tablet";
+
 export const isValidBrowser = browser.satisfies({
   chrome: ">=115",
   edge: ">=115",
