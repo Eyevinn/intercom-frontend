a229fb6fd15f3760106fb30407b433e635cf603f
Sandra Larsson <Sandra.larsson@eyevinn.se>
2025-01-07 14:57:07 +0100

    fix: show volume control on supported devices (#259)

--- src/bowser.ts
+++ src/bowser.ts
@@ -4,8 +4,14 @@ const deviceInfo = Bowser.parse(window.navigator.userAgent);
 const browser = Bowser.getParser(window.navigator.userAgent);
 const browserName = browser.getBrowserName();
 
+const isIOS = (): boolean => {
+  const ua = window.navigator.userAgent.toLowerCase();
+  return /iphone|ipod|ipad/.test(ua);
+};
+
 // platform type, can be either "desktop", "tablet" or "mobile"
 export const isMobile = deviceInfo.platform.type === "mobile";
+export const isIOSMobile = isIOS() && isMobile;
 
 export const isValidBrowser = browser.satisfies({
   chrome: ">=115",
@@ -16,3 +22,21 @@ export const isValidBrowser = browser.satisfies({
 });
 
 export const isBrowserFirefox = browserName.toLowerCase() === "firefox";
+
+// Used because iPads have platform as desktop and browser as Safari
+const detectIpad = (): boolean => {
+  const ua = window.navigator.userAgent.toLowerCase();
+  const platform = window.navigator.platform.toLowerCase();
+  const maxTouchPoints = navigator.maxTouchPoints || 0;
+
+  const result =
+    platform.includes("ipad") ||
+    ua.includes("ipad") ||
+    (maxTouchPoints > 1 && platform.includes("mac") && ua.includes("safari"));
+
+  return result;
+};
+
+export const isIpad = detectIpad();
+
+export const isTablet = deviceInfo.platform.type === "tablet" || isIpad;
