d156e5513d65e1ba5b1b30e8eec15c8034310c00
malmen237 <linda.malm@eyevinn.se>
2025-01-16 16:44:47 +0100

    fix: limited long names and width

--- src/components/landing-page/user-settings-button.tsx
+++ src/components/landing-page/user-settings-button.tsx
@@ -2,15 +2,17 @@ import styled from "@emotion/styled";
 import { FC } from "react";
 import { UserSettingsIcon } from "../../assets/icons/icon";
 import { useStorage } from "../accessing-local-storage/access-local-storage";
+import { isMobile } from "../../bowser";
 
 const UserSettingsWrapper = styled.div`
   position: absolute;
   top: 0;
   right: 0;
   padding: 1.6rem 2rem 1rem 0;
-  font-size: 2rem;
   display: flex;
+  flex-direction: row;
   align-items: center;
+  font-size: 2rem;
   font-weight: bold;
 
   &:hover {
@@ -19,6 +21,13 @@ const UserSettingsWrapper = styled.div`
 
   svg {
     width: 2.5rem;
+    flex-shrink: 0;
+  }
+
+  &.mobile {
+    width: 50vw;
+    justify-content: end;
+    font-size: 1.5rem;
   }
 `;
 
@@ -33,10 +42,14 @@ interface UserSettingsButtonProps {
 export const UserSettingsButton: FC<UserSettingsButtonProps> = (props) => {
   const { onClick } = props;
   const { readFromStorage } = useStorage();
+  const username = readFromStorage("username") || "Guest";
+
+  const truncatedUsername =
+    isMobile && username.length > 25 ? `${username.slice(0, 25)}...` : username;
 
   return (
-    <UserSettingsWrapper onClick={onClick}>
-      <Username>{readFromStorage("username") || "Guest"}</Username>
+    <UserSettingsWrapper className={isMobile ? "mobile" : ""} onClick={onClick}>
+      <Username>{truncatedUsername}</Username>
       <UserSettingsIcon />
     </UserSettingsWrapper>
   );
