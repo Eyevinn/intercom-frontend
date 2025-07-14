5cced9ab3fb8b8e12bee3accea1d34c8113d6e9c
Jonas Birmé <jonas.birme@eyevinn.se>
2025-07-14 22:08:04 +0200

    chore: use OSC_HOSTNAME for API_URL if set

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,6 +4,12 @@ LISTENPORT="${PORT:-3000}"
 sed -i "s/listen\s*8080;/listen $LISTENPORT;/" /etc/nginx/conf.d/default.conf
 API_URL="${MANAGER_URL:-/}"
 
+if [ ! -z "$OSC_HOSTNAME" ]; then
+  API_URL="https://$OSC_HOSTNAME/"
+fi
+
+echo "VITE_BACKEND_URL=$API_URL"
+
 VITE_BACKEND_URL=$API_URL yarn build && \
   cp -r /app/dist/* /usr/share/nginx/html/ && \
   nginx -g 'daemon off;'
