dc02e3b47b2bca262cc7a9a6fb6091971e2ad9ce
Rohan <71641133+Rohanhos@users.noreply.github.com>
2025-11-04 10:26:20 +0100

    fix: do not use full width for production names (#541)

--- src/components/production-list/production-list-components.ts
+++ src/components/production-list/production-list-components.ts
@@ -5,7 +5,7 @@ export const ProductionName = styled.div`
   font-size: 1.4rem;
   font-weight: bold;
   margin-right: 0.5rem;
-  flex: 1;
+  flex: 0 1 auto;
   min-width: 0;
   overflow: hidden;
   white-space: nowrap;
