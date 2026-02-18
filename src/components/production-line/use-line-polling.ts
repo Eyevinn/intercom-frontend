8752e3199c5b4954a812f9529a327b4368f48a4c
lucasmaupin <lucas.maupin@gmail.com>
2026-02-18 14:54:25 +0100

    fix: improve error-handling

--- src/components/production-line/use-line-polling.ts
+++ src/components/production-line/use-line-polling.ts
@@ -17,39 +17,40 @@ export const useLinePolling = ({ callId, joinProductionOptions }: TProps) => {
   useEffect(() => {
     if (!joinProductionOptions) return noop;
 
-    let failure401Count = 0;
+    let consecutiveFailureCount = 0;
     const productionId = parseInt(joinProductionOptions.productionId, 10);
     const lineId = parseInt(joinProductionOptions.lineId, 10);
 
     const interval = window.setInterval(() => {
       API.fetchProductionLine(productionId, lineId)
         .then((l) => {
-          failure401Count = 0;
+          consecutiveFailureCount = 0;
           setLine(l);
         })
-        .catch((err) => {
-          if (err.status === 401) {
-            failure401Count += 1;
-          }
-          // Might want to add another dispatch here for other error codes.
+        .catch(() => {
+          consecutiveFailureCount += 1;
           logger.red(
             `Error fetching production line ${productionId}/${lineId}. For call-id: ${callId}`
           );
-          dispatch({
-            type: "ERROR",
-            payload: {
-              callId,
-              error: new Error(
-                `Could not fetch production line ${productionId}/${lineId}. For call-id: ${callId}`
-              ),
-            },
-          });
-          if (failure401Count >= 10) {
+          if (consecutiveFailureCount >= 5) {
             dispatch({
               type: "ERROR",
               payload: {
                 callId,
-                error: new Error("Line polling stopped after 10 retries."),
+                error: new Error(
+                  `Could not fetch production line ${productionId}/${lineId}. For call-id: ${callId}`
+                ),
+              },
+            });
+          }
+          if (consecutiveFailureCount >= 10) {
+            dispatch({
+              type: "ERROR",
+              payload: {
+                callId,
+                error: new Error(
+                  "Line polling stopped after 10 consecutive failures."
+                ),
               },
             });
             window.clearInterval(interval);
